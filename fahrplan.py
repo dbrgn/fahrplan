4730cc19b09bddea7fd94e4da4f27462f8b9a013
Danilo Bargen <gezuru@gmail.com>
2012-05-03 20:29:04 +0200

    Added license header to fahrplan.py

--- fahrplan.py
+++ fahrplan.py
@@ -1,11 +1,28 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
+# A SBB/CFF/FFS commandline based timetable client.
+# Copyright (C) 2012 Danilo Bargen
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
+
 __title__ = 'fahrplan'
 __version__ = '0.1.0'
 __author__ = 'Danilo Bargen'
 __author_email__ = 'gezuru@gmail.com'
 __license__ = 'GPLv3'
+
 
 import sys
 import argparse
