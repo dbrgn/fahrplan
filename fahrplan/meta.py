730c3a09f52478d333462e2b4637c797d2f6b531
Danilo Bargen <gezuru@gmail.com>
2012-07-23 16:57:05 +0200

    Updated version to v0.1b5

--- fahrplan/meta.py
+++ fahrplan/meta.py
@@ -1,6 +1,6 @@
 """This module contains metadata about the project."""
 title = 'fahrplan'
-version = '0.1b4'
+version = '0.1b5'
 description = 'A SBB/CFF/FFS commandline based timetable client.'
 author = 'Danilo Bargen'
 author_email = 'gezuru@gmail.com'
