1b44b6a6dac65683f5e9bb98e08cc3e5918358f4
Danilo Bargen <gezuru@gmail.com>
2013-09-20 14:39:28 +0200

    Unpinned versions in requirements.txt

--- fahrplan/meta.py
+++ fahrplan/meta.py
@@ -1,6 +1,6 @@
 """This module contains metadata about the project."""
 title = 'fahrplan'
-version = '0.2.1'
+version = '0.2.2'
 description = 'A SBB/CFF/FFS commandline based timetable client.'
 author = 'Danilo Bargen'
 author_email = 'gezuru@gmail.com'
