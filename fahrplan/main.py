bf05e5ed8070301f18bd390ccbe7808e08239a29
Danilo Bargen <mail@dbrgn.ch>
2023-12-02 14:26:37 +0100

    Bump copyright year

--- fahrplan/main.py
+++ fahrplan/main.py
@@ -2,7 +2,7 @@
 # -*- coding: utf-8 -*-
 
 # A SBB/CFF/FFS commandline based timetable client.
-# Copyright (C) 2012-2019 Danilo Bargen
+# Copyright (C) 2012-2023 Danilo Bargen
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
